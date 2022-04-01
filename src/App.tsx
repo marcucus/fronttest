282b14b137a10f0fa6871fa09874648c57a9ccaa
Adrien Marques <adrien.marques@etu.unilim.fr>
2022-04-01 18:19:18 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,31 @@
-import React from "react";
-import { Router } from "@reach/router";
-import { SelectPage } from "./routes/select";
-import { PagesPage } from "./routes/pages";
-import { ModuleProvider } from "./modules/ModuleProvider";
-import { init } from "./redux/store";
-import { Provider } from "react-redux";
-import { Notifications } from "./components/Notifications/Notifications";
-import { FullPageLoader } from "./components/FullPageLoader/FullPageLoader";
-import { CustomIntlProvider } from "./components/CustomIntlProvider/CustomIntlProvider";
-import { DashboardPage } from "./routes/dashboard";
-import { AuthenticationPage } from "./routes/authentication";
-import { StaticInitialization } from "./components/StaticInitialization/StaticInitialization";
-import { startSentry } from "./utils/sentry";
-import { ToolsPage } from "./routes/tools";
-import { RankingPage } from "./routes/ranking";
-import { ToolsBuilderRoute } from "./routes/tools/builder";
-import { ToolsBuilderSlugRoute } from "./routes/tools/builder/[slug]";
-import { TableRank } from "./components/TableRank/TableRank";
-
-
-startSentry();
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GoogleLogin from "react-google-login";
+import googleLogin from "./lib/googleLogin";
 
 function App() {
-  const module = new ModuleProvider().build();
-
-  const { store } = init({}, module);
-
   return (
-    <Provider store={store}>
-      <CustomIntlProvider defautlLang="en">
-        <StaticInitialization>
-          <Router>
-            <AuthenticationPage path="/" />
-            <ToolsPage path="/tools/" />
-            <RankingPage path="/tools/ranking" />
-            <TableRank path="/tools/ranking/table" />
-            <SelectPage path="/tools/indexation/" />
-            <DashboardPage path="/tools/indexation/dashboard/" />
-            <PagesPage path="/tools/indexation/pages/" />
-
-            <ToolsBuilderRoute path="/tools/builder/" />
-            <ToolsBuilderSlugRoute path="/tools/builder/:slug" />
-          </Router>
-
-          <FullPageLoader />
-          <Notifications />
-        </StaticInitialization>
-      </CustomIntlProvider>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <h1>Login with Google</h1>
+        <GoogleLogin clientId={`${process.env.PUBLIC_GOOGLE_CLIENT_ID}`}
+                   buttonText="Login with Google"
+                   onSuccess={async (response) => {
+                     const tokens = await googleLogin(response);
+                     if(!tokens){
+                       alert("Error while logging in with Google")
+                     } else {
+                       //suite connection
+                     }
+                   }}
+                   onFailure={(response) => {
+                     alert("Error while logging in w/Google 2")
+                   }}
+                   cookiePolicy={"single_host_origin"}
+      />
+      </header>
+    </div>
   );
 }
 
