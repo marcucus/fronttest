d857d038d338eeb86e8751522cbcba1e294ef307
Marques <marquesadrien.pro@gmail.com>
2022-05-03 17:54:46 +0200

    connecting back/front

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,8 @@
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
-import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
+import { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
 import Google from "../assets/socials/google.svg";
+import { ReactSession } from 'react-client-session';
 
 export const Login: React.FC<RouteComponentProps> = () => {
     const token:string="";
@@ -26,11 +27,20 @@ export const Login: React.FC<RouteComponentProps> = () => {
           token,
         }
       }
+    function openPopup() {               
+        const url = `http://127.0.0.1:3333/authentication`;
+        const doc = window.open(url, '',
+            `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+scrollbars=no, resizable=no, copyhistory=no`
+        );
+        return doc;
+    }
     return(
         <>
 
             <button
             type="submit"
+            onClick={openPopup}
                 className="inline-flex items-center w-full px-2 py-2 mt-8 text-lg font-medium text-gray-900 ease-in-out bg-white rounded-full shadow-lg cursor-pointer lg:mt-0 hover:bg-gray-50 hover:shadow-xl"
             >
                 <img src={Google} alt="Google" className="flex-shrink-0 w-9" />
