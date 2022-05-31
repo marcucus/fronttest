6e4f9313f8a1d47515db58bb1313843536d3103e
Adrien Marques <adrien.marques@etu.unilim.fr>
2022-05-31 02:15:53 +0200

    save

--- src/components/tableRank.tsx
+++ src/components/tableRank.tsx
@@ -1,11 +1,12 @@
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, Transition, Dialog } from "@headlessui/react";
 import { SelectorIcon, CheckIcon } from "@heroicons/react/solid";
 import { RouteComponentProps } from "@reach/router";
 import axios, { AxiosRequestConfig } from "axios";
 import React, { Fragment, useEffect } from "react";
+import { ExclamationIcon, XIcon } from '@heroicons/react/outline'
 
 export const TableRank: React.FC<RouteComponentProps> = () => {
-
+  const [open, setOpen] = React.useState(false)
   const token = "Bearer "+ localStorage.getItem('userToken');
   const userToken=localStorage.getItem('userToken');
       var head = {
@@ -356,6 +357,80 @@ export const TableRank: React.FC<RouteComponentProps> = () => {
               ) : null}
             </div>
           </div>
+          <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={setOpen}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+
+              <div className="fixed z-10 inset-0 overflow-y-auto">
+                <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  >
+                    <Dialog.Overlay className="relative bg-white rounded-lg w-80 px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
+                      <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                        <button
+                          type="button"
+                          className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                          onClick={() => setOpen(false)}
+                        >
+                          <span className="sr-only">Close</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                      <div className="sm:flex sm:items-start">
+                        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                        </div>
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                            Deactivate account
+                          </Dialog.Title>
+                          <div className="mt-2">
+                            <p className="text-sm text-gray-500">
+                              Are you sure you want to deactivate your account? All of your data will be permanently removed
+                              from our servers forever. This action cannot be undone.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                        <button
+                          type="button"
+                          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                          onClick={() => setOpen(false)}
+                        >
+                          Deactivate
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                          onClick={() => setOpen(false)}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </Dialog.Overlay>
+                  </Transition.Child>
+                </div>
+              </div>
+            </Dialog>
+          </Transition.Root>
           <Listbox value={selected} onChange={setSelected}>
             {({ open }) => (
               <>
@@ -437,22 +512,9 @@ export const TableRank: React.FC<RouteComponentProps> = () => {
                           </td>
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{one.keywords}</td>
                           <td className="pl-7 whitespace-nowrap py-4 pr-3 text-sm text-gray-900">{one.position}</td>
-                          <td className="pl-5 whitespace-nowrap py-4 pr-3 text-sm text-yellow-500"><a href="" className="hover:text-yellow-600">Historique</a></td>
-                          {/*<td className="pl-5 whitespace-nowrap py-4 pr-3 text-sm text-gray-900">{one.json_build_object.pos[0] ? (
-                            one.json_build_object.pos[0].ppos
-                          ):(
-                            '-'
-                          )}</td>
-                          <td className="pl-5 whitespace-nowrap py-4 pr-3 text-sm text-gray-900">{one.json_build_object.pos[1] ? (
-                            one.json_build_object.pos[1].ppos
-                          ):(
-                            '-'
-                          )}</td>
-                          <td className="pl-5 whitespace-nowrap px-3 py-4 text-sm text-gray-500">{one.json_build_object.pos[2] ? (
-                            one.json_build_object.pos[2].ppos
-                          ):(
-                            '-'
-                          )}</td>*/}
+                          <td className="pl-5 whitespace-nowrap py-4 pr-3 text-sm text-yellow-500">
+                            <button onClick={() => setOpen(true)} className="hover:text-yellow-600">Historique</button>
+                          </td>
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{one.url}</td>
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{cut(one.lastcheck)}</td>                                                    
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{cut(one.createdat)}</td>
